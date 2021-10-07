6b461fe7cb509f7b07c1aa388c147846c623ec59
Karan Asthana <karan.asthana1@gmail.com>
2021-10-07 03:17:43 -0400

    populated the year of birth column

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import csv
+import datetime as dt
+
+from numpy import NaN
+
+headerIndexDict = {}
 
 def readCSVFile():
     rows = []
@@ -7,6 +12,8 @@ def readCSVFile():
         header = next(csvreader)
         for row in csvreader:
             rows.append(row)
+    footer = rows[-1]
+    rows = rows[:-1]
     return header, rows
 
 def addNewHeaders(header):
@@ -17,17 +24,52 @@ def addNewHeaders(header):
 
     return header
 
+def cleanup_data(rows):
+    dobIndex = headerIndexDict['BIRTH DATE']
+
+    for row in rows:
+        dateStr = row[dobIndex]
+        dateStrArr = dateStr.split(' ')
+
+        dateStr = dateStrArr[0][:3] + ' ' + dateStrArr[1] + ' ' + dateStrArr[2]
+        row[dobIndex] = dateStr
+
+def populate_rows(header, rows):
+    # populate columns for headers
+    birthDateIndex = headerIndexDict['BIRTH DATE']
+    yearBirthIndex = headerIndexDict['year_of_birth']
+    print('year birth index is ', yearBirthIndex)
+    print(rows[0])
+
+    for row in rows:
+        dob = row[birthDateIndex]
+        if dob == NaN:
+            continue
+
+        row.append(getBirthYear(dob))
+        row.append(getLivedYears())
+        print(row)
+
+def getBirthYear(dob_str):
+    date_time_obj = dt.datetime.strptime(dob_str, '%b %d, %Y')
+    dob_year = date_time_obj.year
+    return dob_year
+
+def getLivedYears():
+    print('test')
+
 def main():
     header, rows = readCSVFile()
 
     header = addNewHeaders(header)
 
-    # Creating a dictionary to store the indices of the new columns
-
-    headerIndexDict = {}
+    # Initializing a dictionary to store the indices of the new columns
     i=0
     for (val) in header:
         headerIndexDict[val] = i
         i = i + 1
 
-    print(headerIndexDict)
+    cleanup_data(rows)
+    populate_rows(header, rows)
+
+main()
